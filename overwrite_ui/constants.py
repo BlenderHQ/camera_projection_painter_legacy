3f63386f20b9a8e7e4dd79f1441e61a4a568a32e
Ivan Perevala <ivan95perevala@gmail.com>
2020-01-19 17:07:52 +0200

    Moved OVERWRITE_UI_MESSAGE

---
+++ overwrite_ui/constants.py
@@ -0,0 +1,6 @@
+# <pep8 compliant>
+
+# The module contains only variables whose values are used as constant values
+
+# Message text that part of the interface has been overwritten
+OVERWRITE_UI_MESSAGE = "Overwriten UI"
