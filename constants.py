e6270998f3d05b2837f6eea910b11cff2dc11e63
Ivan Perevala <ivan95perevala@gmail.com>
2020-01-19 17:06:11 +0200

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,6 +2,5 @@
 
 # The module contains only variables whose values are used as constant values
 
-
 # Name of temporary data for the modifier and UV map of the object
 TEMP_DATA_NAME = "cpp_temp_data"
