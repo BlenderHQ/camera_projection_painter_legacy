1ec0f0798ca7ea4fb7a0cd2bf87dccbacd0b7ebf
Ivan Perevala <ivan95perevala@gmail.com>
2020-01-19 12:31:40 +0200

    Comments

--- gizmos/camera.py
+++ gizmos/camera.py
@@ -1,6 +1,10 @@
 # <pep8 compliant>
 
-if "bpy" in locals():
+# At the moment, there is no way to fully use custom gizmo when
+# splitting a window (see link), therefore, standard
+# https://developer.blender.org/T71941
+
+if "bpy" in locals(): # In case of module reloading
     import importlib
 
     importlib.reload(poll)
@@ -29,6 +33,7 @@ class CPP_GGT_camera_gizmo_group(bpy.types.GizmoGroup):
         return poll.tool_setup_poll(context)
 
     def _create_gizmo(self, camera_ob):
+        """The method adds one gizmo and sets its properties"""
         mpr = self.gizmos.new("GIZMO_GT_primitive_3d")
         props = mpr.target_set_operator("cpp.call_pie")
         props.camera_name = camera_ob.name
@@ -46,22 +51,27 @@ class CPP_GGT_camera_gizmo_group(bpy.types.GizmoGroup):
         scene = context.scene
 
         self._camera_gizmos = {}
-
+        
+        # Gizmo created for every camera in the scene.
         for camera_ob in context.scene.cpp.camera_objects:
             self._create_gizmo(camera_ob)
 
     def refresh(self, context):
         _invalid_camera_gizmos = {}
+        # Fill the dictionary with links to cameras that are deleted
         for camera_ob, mpr in self._camera_gizmos.items():
             try:
                 _name = camera_ob.name
             except ReferenceError:
                 _invalid_camera_gizmos[camera_ob] = mpr
-
+        
+        # Remove the gizmo of these cameras and from the dictionary
         for camera_ob, mpr in _invalid_camera_gizmos.items():
             self._camera_gizmos.pop(camera_ob)
             self.gizmos.remove(mpr)
-
+        
+        # Update the parameters of the gizmo of the existing cameras
+        # and also add new ones if necessary
         for camera_ob in context.scene.cpp.camera_objects:
             if camera_ob in self._camera_gizmos.keys():
                 mpr = self._camera_gizmos[camera_ob]
@@ -71,6 +81,7 @@ class CPP_GGT_camera_gizmo_group(bpy.types.GizmoGroup):
 
     def draw_prepare(self, context):
         preferences = context.preferences.addons[pkg].preferences
+        # Properties concerning only rendering are changed when possible
         for mpr in self.gizmos:
             mpr.color = preferences.gizmo_color[0:3]
             mpr.alpha = preferences.gizmo_color[3]
